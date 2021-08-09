abe950fd61a6750e5311c5361e765901616627ee
YafitSamuel <gyafit642@gmail.com>
2021-08-09 11:41:35 +0300

    commit

---
+++ main.js
@@ -0,0 +1,184 @@
+
+
+
+
+// --------------------------------05.08.21-משימה לסופש עם אסצאלין--------------------------------------
+
+// משימה 1:
+
+// -------א-------
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var min = age[0]
+
+
+// for (var i = 1; i < age.length; i++) {
+//     if (age[i] < min) {
+//         min = age[i]
+//     }
+
+// }
+
+//       console.log(min)
+
+
+
+
+
+
+// -------ב--------
+
+    //   var age = [6, 3, 13, 45, 9, 23]
+    //   var max = age[0]
+     
+      
+    //   for (var i = 1; i < age.length; i++) {
+    //       if (age[i] >max) {
+    //         max = age[i]
+    //       }
+
+    //   }
+    //         console.log(max)
+
+
+      
+// ----------ג--------
+
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var sum = 0 ;
+// for (var i = 1; i < age.length; i++) {
+//     sum = sum + age[i];
+// }
+// console.log(sum)
+
+
+
+// --------ד---------
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var sum = 0 ;
+// for (var i = 1; i < age.length; i++) {
+//     sum = sum + age[i];
+// }
+// console.log(sum/age.length)
+
+
+
+
+
+// --------ה--------
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var num =Number(prompt("enter name"))
+ 
+
+
+
+
+// var numbers=[]
+// var sum=0
+// for(var i=0 ; i<5 ;i++){
+//     var num1= Number(prompt("enter number"))
+//     numbers.push(num1)
+//     sum=numbers[i]+sum
+    
+// }
+// console.log(sum)
+// console.log(numbers)
+
+
+
+
+// -----------------------------------2---------------------------------
+
+
+
+// var nambers = [6, 3, 13, 45, 9, 23]
+
+// var names = [yafit, hila,alona]
+
+
+
+//  ------ -- ------------------------3---------------------------------
+
+// var nambers = [6, 13, 18, 45, 76, 23,58,93]
+// var num1 =nambers[1]
+// var num2=nambers[0]
+// nambers[1]=num2
+// nambers[0]=num1
+// var num3 =nambers[7]
+// nambers[7]=num2
+// nambers[0]=num3
+
+// console.log(nambers)
+
+
+ // ---------------------------------4-----------------------------------
+
+// var num = [5,3,7,8,11]
+// var sum = 0 ;
+// var avg = 0;
+        
+ // for (var i = 0; i < num.length; i++) {
+ //      sum = sum + num[i];
+ // }
+//  console.log(sum)
+   //  console.log(sum/num.length )
+
+      
+  // ---------------------------------5-------------------------------------
+
+//   var num = ["yafit"," roti ", "keren", "kli" ,"lora"]
+  
+//   for(var i=0; i<5; i++){
+
+//   }
+// console.log(num)
+
+
+// -----------------------------------6--------------------------------------
+  
+//   var names = []
+  
+//   for(var i=0; i<5; i++){
+     
+//     names [i]= prompt("enter your name")
+//     console.log(names)
+//   }
+
+// ------------------------------------7----------------------------------------
+
+// var names = ["yafit"," roti ", "keren", "kli" ,"lora" , "gog", "lop" , "asa"]
+// for(var i=0 ; i<names.length ;i++){
+// usernname=prompt("enter your name") 
+// names.push(usernname)
+// console.log(names)
+
+// }
+    
+    //  --- ----------------------------8----------------------------------------
+
+    // var num = []
+    
+    // for(var i=0; i<3 ; i++){
+    //   userNumber [i]=Number(prompt("enter a number"))
+    //   var kefel= 0;
+    //   kefel=userNumber*num[i]
+    // }
+    // console.log(kefel)
+
+
+    // -----------------------------------9-----------------------------------------
+    
+ 
+
+
+
+
+    // -------------------------------------10----------------------------------------
+
+    // -------------------------------------11----------------------------------------
+
+
+    // -------------------------------------12----------------------------------------
